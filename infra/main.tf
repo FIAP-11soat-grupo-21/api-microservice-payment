e4591d4313b06f827ddb2810ffa67e80a5676a2c
JGGrande <joaogabriel7303@gmail.com>
2026-01-09 16:32:04 -0300

    chore: Adiciona 'AWS_SNS_ORDER_ERROR_TOPIC' no terraform

--- infra/main.tf
+++ infra/main.tf
@@ -20,7 +20,8 @@ module "payment_api" {
       DB_USERNAME : data.terraform_remote_state.infra.outputs.rds_postgres_db_username,
       AWS_SQS_CREATE_PAYMENT_QUEUE : data.terraform_remote_state.infra.outputs.sqs_payments_queue_url,
       AWS_SQS_CREATE_KITCHEN_ORDER_QUEUE : data.terraform_remote_state.infra.outputs.sqs_kitchen_orders_queue_url,
-      AWS_SQS_ORDER_ERROR_QUEUE : data.terraform_remote_state.infra.outputs.sqs_payments_order_error_queue_url
+      AWS_SQS_ORDER_ERROR_QUEUE : data.terraform_remote_state.infra.outputs.sqs_payments_order_error_queue_url,
+      AWS_SNS_ORDER_ERROR_TOPIC: "order-error-topic"
     }
   )
 
